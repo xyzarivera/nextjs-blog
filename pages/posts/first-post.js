78e3ab448360ac0fc016d28d159fa2bba393068e
Xyza Rivera <xyzarivera@gmail.com>
2022-06-22 16:27:52 +0800

    add Script React Component in Head component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import Link from 'next/link';
 
 export default function FirstPost() {
@@ -6,6 +7,13 @@ export default function FirstPost() {
     <>
     <Head>
       <title>First Post</title>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
     </Head>
     <h1>First Post</h1>
     <h2>
