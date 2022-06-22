9eda08b9418c728eedcc55a89f5aae6e5b298ee5
Xyza Rivera <xyzarivera@gmail.com>
2022-06-22 16:28:15 +0800

    remove Script component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Script from 'next/script';
 import Link from 'next/link';
 
 export default function FirstPost() {
@@ -7,13 +6,6 @@ export default function FirstPost() {
     <>
     <Head>
       <title>First Post</title>
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
-      />
     </Head>
     <h1>First Post</h1>
     <h2>
